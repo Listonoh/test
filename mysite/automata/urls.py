1bf6ce419c178a931431e1fe230f0a87469f2667
ListonohNB <leestackcz@gmail.com>
2020-03-02 12:57:16 +0100

    better input method

--- mysite/automata/urls.py
+++ mysite/automata/urls.py
@@ -5,5 +5,6 @@ from . import views, textIn
 urlpatterns = [
     path('input/', views.input, name='input'),
     path('', views.index, name='index'),
+    path('index/', views.index, name='index'),
     path('result/', views.result, name="result"),
 ]
